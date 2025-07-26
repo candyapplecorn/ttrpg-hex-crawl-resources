b56bd828308b90187a0082e5e57f510d51613fb3
Joseph Burger <joseph@airkit.com>
2025-07-26 15:56:52 -0500

    feat: added obsfucator

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
   base: './',         // ← use relative paths (fixes GitHub Pages 404s)
-  plugins: [react()],
+  plugins: [react(),
+    obfuscatorPlugin({
+      exclude: [/node_modules/],
+      apply: "build",
+      debugger: true,
+      options: {
+        // your javascript-obfuscator options
+        debugProtection: true,
+        // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
+      },
+    }),
+  ],
   root: '.',
   build: {
     outDir: '../docs',
