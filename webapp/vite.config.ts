5a7587b13ae3886a7ced3b737abf1daabafacf64
Joseph Burger <joseph@airkit.com>
2025-07-26 01:21:32 -0500

    feat: added static web bundle. if i had github actions i wouldnt commit docs. but i dont wanna set it up rn

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',              // where your src/index.html lives
+  build: {
+    outDir: '../docs',         // relative to the root of the repo
+    emptyOutDir: true,         // clear out docs/ on each build
+  },
 })
